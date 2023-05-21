140b146bd3433231dd9d03ef7e01e0b3a3c9ad17
StefanTampu <114590807+StefanTampu@users.noreply.github.com>
2023-05-20 21:52:12 -0400

    Create event listener + func adding new books from form to bRead shelf

--- library.js
+++ library.js
@@ -1,24 +1,34 @@
-let bRead = document.getElementById("b-read");
-let addRead = document.getElementById("add-read");
-let addReading = document.getElementById("add-reading");
+const bRead = document.getElementById("b-read");
+const bReading = document.getElementById("b-reading");
+const addButton = document.querySelectorAll(".add");
+const addRead = document.getElementById("add-read");
+const addReading = document.getElementById("add-reading");
+const form = document.getElementById("nb-form");
 
-let booksFinished = [];
-let booksReading = [];
+let booksReadArr = [];
+let booksReadingArr = [];
 
-function Book(title, author, pages){
+function Book(title, author, pagesRead, pagesTotal){
     this.title = title
     this.author = author
-    this.pages = pages
+    this.pagesRead = pagesRead
+    this.pagesTotal = pagesTotal
 }
 
 
-function addBookToLibrary(title, author, pages){
-    const newBook = new Book(title, author, pages);
-    booksFinished.push(newBook);
-    console.log(booksFinished);
+function addBookToRead(title, author, pagesRead, pagesTotal){
+    const newBook = new Book(title, author, pagesRead, pagesTotal);
+    booksReadArr.push(newBook);
 } 
 
-const displayBooks = (arr) => {
+function addBookToReading(title, author, pagesRead, pagesTotal){
+    const newBook = new Book(title, author, pagesRead, pagesTotal);
+    booksReadingArr.push(newBook);
+} 
+
+
+//Function for displaying books from arrays in library//
+const displayBooks = arr => {
     arr.forEach((element) => {
         const bookContainer = document.createElement("div");
         bookContainer.classList.add("book-container");
@@ -30,13 +40,37 @@ const displayBooks = (arr) => {
         bookAuthor.textContent = `Author: ${element.author}`;
         const bookPages = document.createElement("p");
         bookPages.classList.add("b-pages");
-        bookPages.textContent = `Pages read: ${element.pages}`;
+        bookPages.textContent = `Pages read: ${element.pagesRead} / ${element.pagesTotal}`;
         bookContainer.append(bookTitle, bookAuthor, bookPages);
-        bRead.insertBefore(bookContainer, addRead);
+        if(arr === booksReadArr){
+            bRead.insertBefore(bookContainer, addRead);
+        } else if(arr === booksReadingArr){
+            bReading.insertBefore(bookContainer, addReading);
+        }
     });
 }
 
-addBookToLibrary("Hobbit", "Your", 22);
-addBookToLibrary("Jurnal", "Nicolae", 33);
+addBookToReading("Jurnal", "Nicolae", 33, 50);
 
-displayBooks(booksFinished);
+displayBooks(booksReadArr);
+displayBooks(booksReadingArr);
+
+//Form input converted to book object//
+//const inputToBook = event => {
+    //event.preventDefault();//
+    //const myFormData = new FormData(event.target);
+    //console.log(myFormData);
+//}
+
+function trial(event){
+    event.preventDefault();
+    let f = event.target;
+    const newBook = new Book(f["book-title"].value, f["book-author"].value, f["pages-read"].value, f["book-pages"].value)
+    booksReadArr.push(newBook);
+    while (!bRead.firstElementChild.classList.contains("add")){   //Use firstElementChild instead of firstChild. Otherwise the white space inserted here between divs creates a #text node.
+        bRead.removeChild(bRead.firstChild);
+    }
+    displayBooks(booksReadArr);
+}
+
+form.addEventListener("submit", trial);
